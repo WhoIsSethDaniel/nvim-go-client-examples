5e5091edeaa01c091499bbecef07dbf182261bdf
Seth Daniel <seth@sethdaniel.org>
2020-11-23 21:22:19 -0600

    use %t instead of %s

--- commands.go
+++ commands.go
@@ -14,7 +14,7 @@ type cmdEvalExample struct {
 func exCmd(p *plugin.Plugin, args []string, bang bool, eval *cmdEvalExample) {
 	log.Println("  Args to exCmd:")
 	log.Printf("    arg1: %s\n", args[0])
-	log.Printf("    bang: %s\n", bang)
+	log.Printf("    bang: %t\n", bang)
 	log.Printf("    cwd: %s\n", eval.Cwd)
 	log.Printf("    buffer: %s\n", eval.Bufname)
 }
